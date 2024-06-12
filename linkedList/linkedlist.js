71abee4288b030ccb1e55bd3bbf2e85b2fbb31ba
ankitjsr6 <ankitkumar5327@gmail.com>
2024-06-12 14:24:33 +0530

    added linked list folder.

---
+++ linkedList/linkedlist.js
@@ -0,0 +1,13 @@
+// create a Node
+class Node {
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = null
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null
+    }
+}
