8968f2f376beed19fa4dcb63ade7ad59e1a5c105
ankitjsr6 <ankitkumar5327@gmail.com>
2024-06-12 15:08:19 +0530

    LinkedList insert data at beginning and at end

--- linkedList/linkedlist.js
+++ linkedList/linkedlist.js
@@ -11,3 +11,31 @@ class LinkedList {
         this.head = null
     }
 }
+
+//insert at start
+LinkedList.prototype.insertAtBeginning = function (data) {
+    //create new Node
+    const newNode = new Node(data);
+    //assign to head
+    this.head = newNode;
+}
+
+
+//insert at end
+LinkedList.prototype.insertAtEnd = function (data) {
+    const newNode = new Node(data)
+
+    //check if head is null
+    if (!this.head) {
+        this.head = newNode;
+        return;
+    }
+
+    // find out the last element and then assign newNode.
+    let last = this.head;// assume head as last
+    while (last.next) {
+        last = last.next;
+    }
+    last.next = newNode;
+
+}
