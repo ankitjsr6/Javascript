a55d87caf300f25512e916aa45fcdc0373af5d5c
ankitjsr6 <ankitkumar5327@gmail.com>
2024-06-15 23:14:39 +0530

    BST - insert a node

--- bst/bst.js
+++ bst/bst.js
@@ -10,4 +10,38 @@ class BinarySearchTree {
     constructor() {
         this.root = null;
     }
+
+    insertAtNode(node, newNode)//node -> root
+    {
+        if (node.key > newNode.key) {
+            if (node.left === null) {
+                node.left = newNode
+            }
+            else {
+                this.insertAtNode(node.left, newNode)// recursion
+            }
+        }
+        else {
+            if (node.right === null) {
+                node.right = newNode;
+            }
+            else {
+                this.insertAtNode(node.right, newNode)// recursion
+            }
+        }
+    }
 }
+
+// insertion in binary search tree
+
+BinarySearchTree.prototype.insert = function (key) {
+    // create node
+    const newNode = new BSTNode(key);
+    if (this.root === null) {
+        this.root = newNode;
+    }
+    else {
+        this.insertAtNode(this.root, newNode);
+    }
+}
+
